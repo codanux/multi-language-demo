c982157e6721eea7c6e26ff48684d41344cf8fb4
omer <omer@optimumtasarim.com>
2020-10-17 15:47:25 +0300

    create translations category

--- resources/views/admin/post/create.blade.php
+++ resources/views/admin/post/create.blade.php
@@ -1,4 +1,4 @@
-<x-admin-layout>
+<x-admin-layout :translations="['category' => $category]">
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
--- resources/views/admin/post/edit.blade.php
+++ resources/views/admin/post/edit.blade.php
@@ -1,4 +1,4 @@
-<x-admin-layout :translations="['post' => $post]">
+<x-admin-layout :translations="['category' => $category, 'post' => $post]">
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
