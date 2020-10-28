195c3d9a108a9f360940ddebf4b247857ed8b126
omer <omer@optimumtasarim.com>
2020-10-29 00:26:20 +0300

    Translations added

--- resources/views/admin/post/_card.blade.php
+++ resources/views/admin/post/_card.blade.php
@@ -19,5 +19,22 @@
              </span>
             @endforeach
         </div>
+
+
+        <div>
+            @foreach(config('multi-language.locales') as $locale => $label)
+                @if($trans = $post->translations->firstWhere('locale', $locale))
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    <a href="{{ routeLocalized('admin.post.show', [$trans->category, $trans], $trans->locale) }}"> {!! $trans->name !!}</a>
+                </span>
+                @else
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    <a href="{{ routeLocalized('admin.post.store', [$post->category, 'translation_of' => $post->translation_of, 'locale' => $locale], $locale) }}">
+                        {{ $label }} Add
+                    </a>
+                </span>
+                @endif
+            @endforeach
+        </div>
     </div>
 </div>
--- resources/views/admin/post/category/_card.blade.php
+++ resources/views/admin/post/category/_card.blade.php
@@ -19,5 +19,21 @@
              </span>
             @endforeach
         </div>
+
+        <div>
+            @foreach(config('multi-language.locales') as $locale => $label)
+                @if($trans = $category->translations->firstWhere('locale', $locale))
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    <a href="{{ routeLocalized('admin.category.show', $trans, $trans->locale) }}"> {!! $trans->name !!}</a>
+                </span>
+                @else
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    <a href="{{ routeLocalized('admin.category.store', ['translation_of' => $category->translation_of, 'locale' => $locale], $locale) }}">
+                        {{ $label }} Add
+                    </a>
+                </span>
+                @endif
+            @endforeach
+        </div>
     </div>
 </div>
